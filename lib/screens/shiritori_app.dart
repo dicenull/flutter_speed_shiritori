b1bd9e5acbb912604d1906c64115d08838e94f44
dicenull <dicenull.dev@gmail.com>
2022-08-24 22:42:55 +0900

    しりとりのエラーを例外にしないようにした

--- lib/screens/shiritori_app.dart
+++ lib/screens/shiritori_app.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_speed_shiritori/data/shiritori_word.dart';
@@ -48,11 +49,15 @@ class _Body extends HookConsumerWidget {
         ),
         TextButton(
           onPressed: () async {
-            final preWord = await ref
-                .read(shiritoriProvider)
-                .postShiritori(ShiritoriWord(nextWord: textCtrl.text));
+            try {
+              final preWord = await ref
+                  .read(shiritoriProvider)
+                  .postShiritori(ShiritoriWord(nextWord: textCtrl.text));
 
-            ref.read(wordProvider.notifier).state = preWord;
+              ref.read(wordProvider.notifier).state = preWord;
+            } on DioError catch (e) {
+              print(e.response!.data);
+            }
           },
           child: const Text('送信'),
         ),
